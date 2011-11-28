8f1b6203d200e4e841609abb350446ba8334bed6
Arash Rouhani <miffoljud@gmail.com>
2011-11-28 22:47:12 +0100

    Adding keyboard features to my Fal

--- Fal.hs
+++ Fal.hs
@@ -4,10 +4,13 @@ module Fal (
     module Fal.Fal
   , filterEvent
   , pressed
+  , released
   , repictimate
+  , isHeld
   )
   where
 
+import Graphics.SOE.Gtk hiding (Region, Event)
 import Fal.Fal
 import Fal.Picture (Picture)
 import Fal.Animation (picToGraphic)
@@ -19,9 +22,23 @@ filterEvent f (Event fe) = Event $ map myGuard . fe
         myGuard mv             = Nothing
 
 -- | This event gets triggered when given key is pressed
-pressed :: Char -> Event ()
-pressed c = filterEvent (==c) key ->> ()
+pressed, released :: Char -> Event ()
+pressed  c = filterEvent (==c) key         ->> ()
+released c = filterEvent (==c) keyReleased ->> ()
+
+-- | True when button is held down
+isHeld :: Char -> Behavior Bool
+isHeld c =
+   lift0 False `untilB` pressed  c ->>
+   lift0 True  `untilB` released c ->>
+   isHeld c
 
 -- | Reactimate a Behavior of Pictures
 repictimate :: String -> Behavior Picture -> IO ()
 repictimate title beh = reactimate title (lift1 picToGraphic beh)
+
+-- | "low level" Event for when Key is released on keyboard
+keyReleased :: Event Char
+keyReleased = Event (\(uas,_) -> map getkey uas)
+      where getkey (Just (Key ch False)) = Just ch
+            getkey _                     = Nothing
